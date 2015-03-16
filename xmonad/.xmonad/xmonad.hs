a02008aadb22ce52cca5c47175c52fc3f608ec7a
Arian van Putten <aeroboy94@gmail.com>
2015-03-17 00:16:45 +0100

    update xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -7,15 +7,17 @@ import Graphics.X11.ExtraTypes.XF86
 myConfig = defaultConfig { terminal           = "gnome-terminal"
                          , modMask            = mod4Mask
                          , borderWidth        = 1
-                         , normalBorderColor  = "#550000"
-                         , focusedBorderColor = "#ff0000"
+                         , normalBorderColor  = "#333333"
+                         , focusedBorderColor = "#d64937"
                          } `additionalKeys` myKeys
 
 myKeys = [((noModMask,xF86XK_MonBrightnessUp)  , spawn "xbacklight +10")
-         ,((noModMask,xF86XK_MonBrightnessDown), spawn "xbacklight -10")]
+         ,((noModMask,xF86XK_MonBrightnessDown), spawn "xbacklight -10")
+         ,((mod4Mask.|.shiftMask,xK_o), spawn "passmenu")
+         ]
 
 myBar = "xmobar"
-myPP  = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+myPP  = xmobarPP { ppCurrent = xmobarColor "#ff00ff" "#333333" . wrap "<" ">" }
 
 main = do
   c <- statusBar myBar myPP toggleStrutsKey myConfig
